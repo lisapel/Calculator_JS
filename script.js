2c2c821396171b19715134c25db3f7ddce49398e
lisapel <lisa.peldan@gmail.com>
2023-03-26 16:01:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const buttons = document.querySelectorAll('.buttons button');
+const screen = document.getElementById('screen');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.id !== 'ce' && button.id !== 'del' && button.id !== 'equals' && button.id!=='sqrt') {
+            screen.value += button.textContent;
+        }
+    });
+});
+
+function validateInput(input) {
+    const allowedChars = /^[0-9+\-*/().√]+$/;
+    return allowedChars.test(input);
+}
+
+function calc() {
+    const screen = document.getElementById('screen');
+
+    if (!validateInput(screen.value)) {
+        screen.value = "Invalid input";
+        return;
+    }
+    try {
+        const result = math.evaluate(screen.value);
+        screen.value = result;
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function sqrt(){
+    const screen = document.getElementById('screen').value;
+    document.getElementById('screen').value = math.sqrt(screen);
+}
+
+
+function deleteAll() {
+    document.getElementById('screen').value = '';
+
+}
+function deleteToken() {
+    const screen = document.getElementById('screen');
+    screen.value = screen.value.slice(0, -1);
+}
